adabb53e7f23e6fb04ebbc2d4e1e9e5ac340201b
stamley <axellystam@gmail.com>
2023-01-01 15:07:13 +0100

    Added offset and fixed display to be on beat. Rescheduling maybe too much.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <!--<Instruments :polid = this.polid></Instruments>-->
   <Circles :polid = this.polid></Circles>
-  <Play :polid = this.polid></Play>
+  <!--<Play :polid = this.polid></Play>-->
   <!--<img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>-->
 </template>
@@ -10,14 +10,14 @@
 import Polid from './Polid';
 //import Instruments from './components/Instruments.vue';
 import Circles from './components/Circles.vue'
-import Play from './components/Play.vue'
+//import Play from './components/Play.vue'
 
 export default {
   name: 'App',
   components: {
     //Instruments,
     Circles,
-    Play,
+    //Play,
   },
   setup(){
     const polid = new Polid();
