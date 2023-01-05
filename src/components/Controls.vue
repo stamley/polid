e49d7f41b7f5706f211ce8e05bec0ca7c0d568b9
stamley <axellystam@gmail.com>
2023-01-05 13:19:00 +0100

    Fixed controls text

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,33 +1,48 @@
 <template>
     <div class="controls">
-        <div class="controls-text">
-            <p>&lt; OR > = CHANGE ACTIVE INSTRUMENT</p> 
-            <p>SHIFT + &lt; OR > = CHANGE BPM</p>
+        <div class="controls-text" v-if="active">
+            <p>	<!--Left--><i>&#x2190;</i> OR <!--Right--><i>&#x2192;</i> = CHANGE ACTIVE INSTRUMENT</p> 
+            <p>SHIFT + <i>&#x2190;</i> OR <i>&#x2192;</i> = CHANGE BPM</p>
             <p>ENTER = ADD INSTRUMENT</p>
             <p>BACKSPACE = REMOVE INSTRUMENT</p>
-            <p><i>&#xf062;</i> OR v = CHANGE NR OF STEPS</p>
-            <p>SHIFT + ^ OR v = INCREASES OR DECREASES PULSES</p>
-            <p>CTRL + ^ OR v = CHANGE OFFSET</p>
+            <p>	<!--Up--><i>&#x2191;</i> OR <!--down--><i>&#x2193;</i> = CHANGE NR OF STEPS</p>
+            <p>SHIFT + <i>&#x2191;</i> OR <i>&#x2193;</i> = INCREASES OR DECREASES PULSES</p>
+            <p>CTRL + <i>&#x2191;</i> OR <i>&#x2193;</i> = CHANGE OFFSET</p>
             <p>SPACE = START SEQUENCE</p>      
         </div>
     </div>
+    <p class="show-text" @click="() => { active = !active }">show controls</p>
 </template>
 <style>
 .controls {
     position: absolute;
     left: 2%;
-    top: 40%;
+    top: 34%;
     z-index: 100;
+    width: 24%;
+    height: 40%;
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 1.7vh;
+    font-weight: bold;
 }
-.controls-text {
-   font-family: 'Courier New', Courier, monospace;
+.show-text {
+    text-decoration: underline1;
+    bottom: 2%;
+    left: 2%;
+    position: absolute;
 }
 p {
     color: rgb(0, 240, 0)
+}
+i {
+    font-size: 3.4vh;
 }
 </style>
 <script>
 export default {
     name: "Controls-View",
+    data() {
+        return { active: false }
+    },
 }
 </script>
